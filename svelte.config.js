2f4455395aaeb727282b7f0e2eda488e8e246617
fravigevani <vigevanifrancesca@gmail.com>
2026-04-29 00:42:17 +0200

    normalize base styles

--- svelte.config.js
+++ svelte.config.js
@@ -5,7 +5,7 @@ import { mdsvex } from 'mdsvex';
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	extensions: ['.svelte', '.md'],
-	preprocess: [vitePreprocess(), mdsvex({ extensions: ['.md'] })], 
+	preprocess: [vitePreprocess(), mdsvex({ extensions: ['.md'] })],  
 	compilerOptions: {
 		// Force runes mode for the project, except for libraries. Can be removed in svelte 6.
 		runes: ({ filename }) => (filename.split(/[/\\]/).includes('node_modules') ? undefined : true)
