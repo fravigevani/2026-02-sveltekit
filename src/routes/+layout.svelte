0888194fc4de233e9b2206e0f7caa8dd64cf69cc
fravigevani <vigevanifrancesca@gmail.com>
2026-04-29 00:57:07 +0200

    add custom font

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -8,6 +8,19 @@
 
 <svelte:head>
 	<link rel="icon" href={favicon} />
+
+	<!-- satoshi font-->
+	<link rel="preconnect" href="https://api.fontshare.com/">
+	<link href="https://api.fontshare.com/v2/css?f[]=satoshi@1,2&display=swap" rel="stylesheet">
+
+	<!-- inter font-->
+
+	<!-- <link rel="preconnect" href="https://fonts.googleapis.com">
+	<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+	<link href="https://fonts.googleapis.com/css2?family=Inter:ital,opsz,wght@0,14..32,100..900;1,14..32,100..900&display=swap" rel="stylesheet"> -->
+	
+	<link rel="preconnect" href="https://rsms.me/">
+	<link rel="stylesheet" href="https://rsms.me/inter/inter.css">
 </svelte:head>
 
 {@render children()}
